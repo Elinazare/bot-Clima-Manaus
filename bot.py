9b018ecefb02bd50d7daef3e8d0ffdd25204d012
Eli <elinazare2911@gmail.com>
2024-09-19 17:05:27 -0400

    deletei uma pasta

--- bot.py
+++ bot.py
@@ -1,27 +1,3 @@
-"""
-WARNING:
-
-Please make sure you install the bot dependencies with `pip install --upgrade -r requirements.txt`
-in order to get all the dependencies on your Python environment.
-
-Also, if you are using PyCharm or another IDE, make sure that you use the SAME Python interpreter
-as your IDE.
-
-If you get an error like:
-```
-ModuleNotFoundError: No module named 'botcity'
-```
-
-This means that you are likely using a different Python interpreter than the one used to install the dependencies.
-To fix this, you can either:
-- Use the same interpreter as your IDE and install your bot with `pip install --upgrade -r requirements.txt`
-- Use the same interpreter as the one used to install the bot (`pip install --upgrade -r requirements.txt`)
-
-Please refer to the documentation for more information at
-https://documentation.botcity.dev/tutorials/python-automations/web/
-"""
-
-
 # Import for the Web Bot
 from botcity.web import WebBot, Browser, By
 
@@ -53,14 +29,13 @@ def extrair_dados(bot):
         
         print(f'Dia: {dia_semana}')
         print(f'Temperatura:\nMax= {temp_max} / Min = {temp_min}')
+        print('-------------------------')
         
         if cont==8:
             break
 
 
 def main():
-    # Runner passes the server url, the id of the task being executed,
-    # the access token and the parameters that this task receives (when applicable).
     maestro = BotMaestroSDK.from_sys_args()
     ## Fetch the BotExecution with details from the task, including parameters
     execution = maestro.get_execution()
@@ -73,13 +48,12 @@ def main():
     # Configure whether or not to run on headless mode
     bot.headless = False
 
-    # Uncomment to change the default Browser to Firefox
+   
     bot.browser = Browser.CHROME
 
-    # Uncomment to set the WebDriver path
+   
     bot.driver_path =ChromeDriverManager().install()
 
-    # Opens the BotCity website.
     bot.browse("https://www.google.com")
 
     try:
@@ -93,22 +67,8 @@ def main():
         
     finally:
         bot.wait(3000)
-        
-    # Wait 3 seconds before closing
-    bot.wait(3000)
 
-    # Finish and clean up the Web Browser
-    # You MUST invoke the stop_browser to avoid
-    # leaving instances of the webdriver open
     bot.stop_browser()
-
-    # Uncomment to mark this task as finished on BotMaestro
-    # maestro.finish_task(
-    #     task_id=execution.task_id,
-    #     status=AutomationTaskFinishStatus.SUCCESS,
-    #     message="Task Finished OK."
-    # )
-
 
 def not_found(label):
     print(f"Element not found: {label}")
